617123fbea6be285555c5f1b4713b42b8fa20775
DmitryNikolin <ni-ko-dim@ukr.net>
2022-10-01 23:13:59 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,8 @@
-import Player from '@vimeo/player';
+import player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
+const player = new player(iframe);
 const KEY = 'videoplayer-current-time';
 
 afterReboot();
