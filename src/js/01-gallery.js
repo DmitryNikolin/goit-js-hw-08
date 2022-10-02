81645c400415897b74140217285b156c557710b3
DmitryNikolin <ni-ko-dim@ukr.net>
2022-10-02 13:01:37 +0300

    package.json

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,9 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from '../../node_modules/simplelightbox/src/simple-lightbox.js';
+import '../../node_modules/simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
 
 const gallery = document.querySelector('.gallery');
